46ee8ce6b93413decdd150e448cb7770439ddfdf
olexey27 <allxyog@gmail.com>
2024-12-02 22:11:25 +0100

    Tailwind config, fonts, global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,18 @@ module.exports = {
 		lg: "960px",
 		xl: "1200px",
 	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
   	extend: {
+		colors: {
+			primary: "#1c1c22",
+			accent: {
+				DEFAULT: "#00ff99",
+				hover: "#00e187",
+			}
+		},
+
 		keyframes: {
 			"accordion-down": {
 				from: { height: "0" },
@@ -30,54 +41,11 @@ module.exports = {
 				to: { height: "0" },
 			},
 		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+  		animation: {
+			"accordion-down": "accordion-down 0.2s easy-out",
+			"accordion-up": "accordion-up 0.2s easy-out",
+		},
+  	},
   },
   plugins: [require("tailwindcss-animate")],
 };
