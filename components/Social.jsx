bb6a8ce2ef1ada984a9a16b0e9a228b7990d7700
olexey27 <allxyog@gmail.com>
2024-12-15 23:53:13 +0100

    add socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaTwitter />, path: "" },
-    { icon: <FaInstagram />, path: "" },
+    { icon: <FaGithub />, path: "https://github.com/olexey27" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/alexej-krasnokutskij-3a2920235/" },
+    { icon: <FaTwitter />, path: "https://x.com/olexey27" },
+    { icon: <FaInstagram />, path: "https://www.instagram.com/olexey27/" },
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
