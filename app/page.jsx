e4b5ce2d1f3165b36f80fd661eeb63be61047f39
olexey27 <allxyog@gmail.com>
2024-12-16 23:45:44 +0100

    photo fix

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl: text-left order-2 xl:order-none">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Junior iOS Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Alexej Krasnokutskij</span>
